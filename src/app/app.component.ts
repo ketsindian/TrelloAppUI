b93d489fbf425866507788c11e35711015ea3231
ketan <ketansomvanshi007@gmail.com>
2020-03-09 15:09:22 +0530

    added logout functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from './services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'trello-app';
+export class AppComponent implements OnInit{
+  title = 'Trello App';
+  constructor(private authService:AuthService,private router:Router) { }
+
+  isLoggedin:boolean=false;
+
+  ngOnInit(): void {
+    this.isloggedin();
+  }
+
+  isloggedin(){
+    if(this.router.routerState.snapshot.url===""){
+      return false;
+      // this.isLoggedin=false;
+    }else{
+      if(this.authService.getToken){
+        return true;
+        // this.isLoggedin=true;
+      }else{
+        return false;
+        // this.isLoggedin=false;
+      }
+    }
+  }
+
+  logOut(){
+    this.authService.logout();
+
+  }
+
 }
